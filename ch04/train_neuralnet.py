a2c65e05b47bc28481d6ee695a7f80cab478cb4c
yk0817 <em072029@gmail.com>
2017-04-23 20:00:00 +0900

    途中

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -1,1 +1,8 @@
+# coding: utf-8
+
+import sys, os
+sys.path.append(os.pardir)
 import numpy as np
+import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
+from two_layer_net import TwoLayerNet
